4e62259c6a55e750a64b279a397571dcde5621de
Santosh Kumar Patro <patrosantoshkumar1212@gmail.com>
2022-07-26 21:04:18 +0530

    added user

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.full_name
