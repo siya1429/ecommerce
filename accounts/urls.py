5665d66fd93170c13e2876daa5c36a9c3075a704
Santosh Kumar Patro <patrosantoshkumar1212@gmail.com>
2022-08-06 20:40:00 +0530

    added product list filter

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('login/',views.login_view,name='login'),
-  path('profile/',views.profile,name='profile'),
-  path('register/',views.register,name='register'),
+  path('login/', views.login_view, name='login'),
+  path('profile/', views.profile, name='profile'),
+  path('register/', views.register, name='register'),
   path('logout/', views.logout_view, name='logout')
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Category, Product
 
 # Create your views here.
 def product_list(request):
+  category = request.GET.get('category', None)
+
   products = Product.objects.filter(is_active=True)
-  return render(request,'products/product_list.html', { 'products': products })
+  if category:
+    products = products.filter(category__name=category)
+
+  categories = Category.objects.all()
+  return render(request,'products/product_list.html', { 'products': products, 'categories': categories })
 
 def product_view(request, id):
   try:
     product = Product.objects.get(id=id, is_active=True)
     return render(request,'products/product_view.html', { 'product': product })
   except Product.DoesNotExist:
-    return redirect('product_list') 
+    return redirect('product_list')
   
